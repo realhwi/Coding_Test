cac2c998da170b1b0549d22fde3ba2ff3acd49fc
realhwi <wlsgnl49@gmail.com>
2024-09-22 13:04:30 +0900

    5_1 (문자열)문자와 문자열

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,28 +9,14 @@
 
 int main() {
 
-    // 과목 개수 n, 점수 배열 m, 최대점수 max
-    int n,max=0;
-    scanf("%d",&n);
+    //단어 s와 정수 i가 주어졌을 때 s의 i번째 출력
+    char s[100];
+    int i;
 
-    // 동적 배열 
-    int* m = (int*)malloc(n * sizeof(int));
+    scanf("%s",s);
+    scanf("%d",&i);
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &m[i]);
-        if (m[i] > max) {
-            max = m[i];
-        }
-    }
-
-    // 평균 값 avg 
-    float avg =0;
-    for (int i = 0; i < n; i++) {
-        // 배열 순회하면서 avg에 우측 값 계산 후 순회할때마다 더하기 
-        avg += (float)m[i] / max * 100;
-    }
-
-    printf("%f\n",avg/n);
+    printf("%c",s[i-1]); //인덱스 0부터니까 
 
     return 0;
 }
