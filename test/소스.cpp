8a86242d28f596e97b46a9a7d863ff61a0ad06d6
realhwi <wlsgnl49@gmail.com>
2024-09-25 19:56:22 +0900

    5-5 (문자열) 11720번_숫자의 합

--- test/소스.cpp
+++ test/소스.cpp
@@ -10,9 +10,21 @@
 
 int main() {
     
-    char asc;
-    scanf("%c", &asc); 
-    printf("%d\n",asc); //char형을 int형으로 접근하면 아스키 코드가 됨 
-    
+    int n,temp=0; 
+    char input[100];
+
+    // 숫자 개수 입력 받기
+    scanf("%d",&n);
+    fseek(stdin, 0, SEEK_END);
+
+    // 공백 없이 이어진 문자열 입력받기
+    scanf("%s", input);
+
+    for (int i = 0; i < n; i++) {
+        temp += input[i]-'0'; //input[i]는 char 타입 값이라 아스키 코드'0'을 빼줘야 정수로 변환됨  
+    }
+
+    printf("%d\n", temp);
+
     return 0;
 }
