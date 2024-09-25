e25412c80e2703bb99b7b972fc5a872294705a03
realhwi <wlsgnl49@gmail.com>
2024-09-25 19:36:28 +0900

    5-4 (문자열) 아스키코드

--- test/소스.cpp
+++ test/소스.cpp
@@ -10,19 +10,9 @@
 
 int main() {
     
-    int n;//몇번 입력할지
-    char input[100];
-
-    scanf("%d",&n);
-
-
-    while (n>0) // 여기서 n--해도됨, n번까지 반복 
-    {
-        scanf("%s",input); // 입력받은 문자열을 배열 input에 저장 
-
-        printf("%c%c\n",input[0],input[strlen(input)-1]); // 마지막 null까지 구하는 것이 strlen인데, -1 해주면 마지막 문자 출력됨  
-        n--; //n감소 
-    }
+    char asc;
+    scanf("%c", &asc); 
+    printf("%d\n",asc); //char형을 int형으로 접근하면 아스키 코드가 됨 
     
     return 0;
 }
