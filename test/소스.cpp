a5a28259e251a3e45b3cf9053a04cccfb2089b20
realhwi <wlsgnl49@gmail.com>
2024-09-27 17:27:24 +0900

    5-6 (문자열) 10809 알파벳 찾기

--- test/소스.cpp
+++ test/소스.cpp
@@ -10,21 +10,30 @@
 
 int main() {
     
-    int n,temp=0; 
-    char input[100];
+   //단어 s 
+   char s[100];
+   scanf("%s", s);
 
-    // 숫자 개수 입력 받기
-    scanf("%d",&n);
-    fseek(stdin, 0, SEEK_END);
+   char w[26]; //알파벳 위치 저장 배열 
 
-    // 공백 없이 이어진 문자열 입력받기
-    scanf("%s", input);
+   // ASCII 코드 97(a)부터 122(z)까지 dd 배열에 저장
+   for (int i = 0; i < 26; i++) {
+       w[i] = -1; // -1로 초기화 
+   }
 
-    for (int i = 0; i < n; i++) {
-        temp += input[i]-'0'; //input[i]는 char 타입 값이라 아스키 코드'0'을 빼줘야 정수로 변환됨  
-    }
+   // s의 문자열 끝까지 반복 
+   for (int i = 0; s[i] != '\0'; i++) {
+       int index = s[i] - 'a'; 
+       // 'a'=97(아스키코드)부터 시작하는 인덱스 구하기
+       // 이 인덱스 번호에 i번을 업데이트 시켜야 몇번째에 몇번의 단어가 나왔는지 체크 할 수 있음 
+       if (w[index] == -1) { // 해당 알파벳이 처음 등장하는 경우에만 저장
+           w[index] = i; // index번에 몇번째에 알파벳이 나왔는지 값 업데이트 
+       }
+   }
 
-    printf("%d\n", temp);
-
+   for (int i = 0; i < 26; i++) {
+       printf("%d ", w[i]); // 업데이트된 w배열을 전체 출력하기 
+   }
+   
     return 0;
 }
