99083b3bdb53baba4e463ccd910ce92804795f8b
realhwi <wlsgnl49@gmail.com>
2024-09-27 18:07:32 +0900

    5-7 (문자열) 2675 문자열 반복

--- test/소스.cpp
+++ test/소스.cpp
@@ -10,30 +10,26 @@
 
 int main() {
     
-   //단어 s 
-   char s[100];
-   scanf("%s", s);
+    int n; // 총 몇번 할건지 
+    scanf("%d",&n);
 
-   char w[26]; //알파벳 위치 저장 배열 
+    for (int i = 0; i < n; i++) {
+        int M; // 반복할 횟수 
+        char s[100]; // 입력받을 문자 
 
-   // ASCII 코드 97(a)부터 122(z)까지 dd 배열에 저장
-   for (int i = 0; i < 26; i++) {
-       w[i] = -1; // -1로 초기화 
-   }
+        scanf("%d %s", &M, s);
+        
+        int len = strlen(s); //문자열 길이 
 
-   // s의 문자열 끝까지 반복 
-   for (int i = 0; s[i] != '\0'; i++) {
-       int index = s[i] - 'a'; 
-       // 'a'=97(아스키코드)부터 시작하는 인덱스 구하기
-       // 이 인덱스 번호에 i번을 업데이트 시켜야 몇번째에 몇번의 단어가 나왔는지 체크 할 수 있음 
-       if (w[index] == -1) { // 해당 알파벳이 처음 등장하는 경우에만 저장
-           w[index] = i; // index번에 몇번째에 알파벳이 나왔는지 값 업데이트 
-       }
-   }
+        //문자열 끝까지 반복 
+        for (int j = 0; j < len; j++) {
+            // s의 j인덱스를 M번 반복해서 출력 
+            for (int k = 0; k < M; k++) {
+                printf("%c", s[j]);
+            }
+        }
+        printf("\n"); 
+    }
 
-   for (int i = 0; i < 26; i++) {
-       printf("%d ", w[i]); // 업데이트된 w배열을 전체 출력하기 
-   }
-   
     return 0;
 }
