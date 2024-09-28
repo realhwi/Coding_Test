3a2e2d8cd0fad1f75bd321be1879e8d1bfc54038
realhwi <wlsgnl49@gmail.com>
2024-09-28 20:56:55 +0900

    5-8 (문자열) 1152번 단어의 개수

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,27 +9,26 @@
 
 
 int main() {
-    
-    int n; // 총 몇번 할건지 
-    scanf("%d",&n);
 
-    for (int i = 0; i < n; i++) {
-        int M; // 반복할 횟수 
-        char s[100]; // 입력받을 문자 
+    char input[1000];
+    int count =0,len;
 
-        scanf("%d %s", &M, s);
-        
-        int len = strlen(s); //문자열 길이 
+    scanf("%[^\n]",input);
+    len = strlen(input);
 
-        //문자열 끝까지 반복 
-        for (int j = 0; j < len; j++) {
-            // s의 j인덱스를 M번 반복해서 출력 
-            for (int k = 0; k < M; k++) {
-                printf("%c", s[j]);
-            }
-        }
-        printf("\n"); 
+    //만약 첫번째 입력이 공백일 경우 0을 출력
+    if (len == 1 && input[0] == ' ') {
+        printf("%d", count);
+
+        return 0;
     }
+    // 문자열 끝 길이까지 반복하는데 공백이 있을 경우 count++
+    for (int i = 0; i < len; i++) {
+        if (input[i] == ' ') {
+            count++;
+        }
+    }
+    printf("%d",count+1);
 
     return 0;
 }
