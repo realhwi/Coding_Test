b7130ab8331112b802325af043ca5475a9d0ed4c
realhwi <wlsgnl49@gmail.com>
2024-10-07 13:31:01 +0900

    6-5 (심화) 1157번 단어공부

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,18 +9,42 @@
 
 
 int main() {
-	char a[100];
-	scanf("%s", a);
+	
+	char word[100];
+	scanf("%s", word); // 입력된 문자열 
 
-	int len = strlen(a);
+	int a[26]={0}; // 알파벳 배열 
+	int max = 0; // 가장 많이 등장한 알파벳 빈도수 
+	int len = strlen(word); // 문자열 길이 
+	char result = '?'; // 최종 결과 출력 문자 
 
-	for (int i = 0; i < len/2; i++) {
-		if (a[i] != a[len-i-1]) { // 문자열 중앙부터 양 문자열 같은지 점검 
-			printf("0");
-			return 0; //즉시 종료 
+	//소문자 'a'=97, 대문자 'A' = 65
+	for (int i = 0; i < len; i++) {
+		// 소문자라면 	
+		if (word[i]>='a') {
+			a[word[i]-'a']++; 
+			// word[i]에서 '97'을 빼서 몇번째 인덱스인지 구한뒤, 이 인덱스에 값 증가 
+		}
+		// 대문자라면 
+		else {
+			// word[i]에서 '65'를 빼서 몇번째 인덱스인지 구한뒤, 이 인덱스에 값 증가 
+			a[word[i]-'A']++;
 		}
 	}
-	printf("1");
+
+	//  a[i] 인덱스 순회해서 가장 많이 등장한 알파벳 찾기
+	for (int i = 0; i < 26; i++) {
+		if (a[i] == max) { // 변경된(?) max의 값과 a[i]의 값이 같을 경우
+			result = '?';  // 중복되니까 ?를 출력 
+		}
+		else if (a[i] > max) {
+			max = a[i]; // a[i]의 값을 max로 업데이트 
+			result = 'A' + i; // 아스키로 변환해서 문자열 출력 
+		}
+	}
+
+	// 최종 결과 출력
+	printf("%c", result);
 
 	return 0;
 }
