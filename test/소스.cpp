5c4b666f9845d874e91b97cb791de39843d2e174
realhwi <wlsgnl49@gmail.com>
2024-10-10 15:21:18 +0900

    6-7 (심화) 그룹 단어 체커 수정

--- test/소스.cpp
+++ test/소스.cpp
@@ -11,31 +11,31 @@
 int main() {
 
     // 입력받은 문자에서 중복되는 알파벳이 없어야 그룹단어임 
-    char word[101];
-    int n, count = 0;
+    char word[101]; // 입력 문자 
+    int n; // 문자 몇번 입력할건지 
+    int count = 0; // 그룹단어 체크해서 출력할 문자
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
         scanf("%s", word);
 
-        int isGroup = 1;
-        for (int k = 0; word[k] != '\0'; k++) {
-            // 연속된 문자는 건너뜀
-            if (k > 0 && word[k] == word[k - 1]) {
-                continue;
+        char a[26]={0}; // 알파벳으로 중복 체크 
+        bool isGrop = true;
+
+        for (int k = 0; word[k] != '\0'; k++) {  // word의 문자열 만큼 반복문 실행 
+            if(k>0 && word[k]==word[k-1]){
+                continue;// for문 루프에서 연속된 단어는 넘어가기
             }
 
-            // 이후에 같은 문자가 있는지 확인
-            for (int j = k + 1; word[j] != '\0'; j++) {
-                if (word[k] == word[j]) {
-                    isGroup = 0;
-                    break;
-                }
+            if (a[word[k] - 'a' == 1]) { // for 루프에서 문자 순서대로 인덱스에서 아스키값 빼줘서 인덱스 구하고 그 인덱스의 값이 1인적 있으면 루프 탈출 
+                isGrop = false;
+                break;
             }
-        }
+            a[word[k]-'a']=1; // 현재 문자를 기록 
 
-        if (isGroup) {
-            count++;
+            if (isGrop) { // isGrop이 true 일때만 count 올리기 
+                count++;
+            }
         }
     }
 
