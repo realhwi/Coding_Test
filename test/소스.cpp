9b281a8e0ca2289c9a5ad2ab87b7a79f9b7353de
realhwi <wlsgnl49@gmail.com>
2024-10-17 12:16:02 +0900

    7-4 (2차원 배열) 2563번 색종이

--- test/소스.cpp
+++ test/소스.cpp
@@ -7,23 +7,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 int main() {
+    
+    //100*100 도화지 
+    int canvas[100][100]={0,};
+    // 색종이 개수
+    int n;
 
-    char A[5][15] = { 0, }; // 세로 5, 가로 15까지 
+    scanf("%d",&n);
 
-    for (int i = 0; i < 5; i++) {
-        scanf("%s", A[i]); // i번째 인덱스에 두번째 인덱스 값 넣기
-    }
+    // 색종이의 시작 좌표 입력 받아서 paper 배열에 표시
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
 
-    // 열 출력하기 (세로로 읽기)
-    for (int j = 0; j < 15; j++) { // 열 (문자열의 길이 범위)
-        for (int i = 0; i < 5; i++) { // 행 (각 단어)
-            if (A[i][j] != '\0') { // 유효한 문자만 출력 (널 문자가 아닌 경우)
-                printf("%c", A[i][j]);
+        // 10x10 크기의 색종이를 해당 좌표에 표시
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+                // 겹치는 부분을 고려해서 1로 표시 
+                canvas[x + j][y + k] = 1;
             }
         }
     }
+
+    // 도화지에 덮인 색종이 면적 계산
+    int Area = 0;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (canvas[i][j] == 1) {
+                Area++;
+            }
+        }
+    }
+
+    printf("%d\n", Area);
 
     return 0;
 }
