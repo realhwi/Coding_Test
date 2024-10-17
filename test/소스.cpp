b967cee1057629db0174b64aeac1865940c1d47e
realhwi <wlsgnl49@gmail.com>
2024-10-17 14:31:06 +0900

    8-1 (일반 수학1) 2745번 진법 변환

--- test/소스.cpp
+++ test/소스.cpp
@@ -7,40 +7,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int main() {
-    
-    //100*100 도화지 
-    int canvas[100][100]={0,};
-    // 색종이 개수
-    int n;
+    char N[100]; // B진법 수를 입력받을 배열
+    int B;       // 진법을 입력받을 변수
 
-    scanf("%d",&n);
+    // 입력: B진법 수 N과 진법 B
+    scanf("%s %d", N, &B);
 
-    // 색종이의 시작 좌표 입력 받아서 paper 배열에 표시
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        scanf("%d %d", &x, &y);
+    int len = strlen(N); 
+    int result = 0;      
+    int pow = 1;         
+    // result는 반환값, pow는 제곱 / Z×B^0 부터 ZXB^len까지   
 
-        // 10x10 크기의 색종이를 해당 좌표에 표시
-        for (int j = 0; j < 10; j++) {
-            for (int k = 0; k < 10; k++) {
-                // 겹치는 부분을 고려해서 1로 표시 
-                canvas[x + j][y + k] = 1;
-            }
+
+    // 뒤에서부터 (가장 낮은 자리부터) 순회하며 10진법으로 변환
+    for (int i = len - 1; i >= 0; i--) {
+        int value; // B진법은 10진법으로 바꾼 값 
+
+        // 현재 문자가 숫자면 해당 숫자의 값을 가져옴
+        if ('0' <= N[i] && N[i] <= '9') {
+            value = N[i] - '0';
         }
+        // 현재 문자가 알파벳이면 A는 10으로 시작 Z는 35로 변환
+        else {
+            value = N[i] - 'A' + 10;
+        }
+
+        // 자릿수 값을 10진법으로 변환하여 누적
+        result += value * pow;
+
+        // 다음 자리의 B^지수를 위해 power를 B배 증가
+        pow *= B;
     }
 
-    // 도화지에 덮인 색종이 면적 계산
-    int Area = 0;
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            if (canvas[i][j] == 1) {
-                Area++;
-            }
-        }
-    }
-
-    printf("%d\n", Area);
+    // 변환된 10진법 값 출력
+    printf("%d\n", result);
 
     return 0;
 }
