13cf94512c1c26c69232e89f7cf5280290860091
realhwi <wlsgnl49@gmail.com>
2024-10-30 20:25:07 +0900

    9-1 (약수,배수,소수) 5086번 배수와 약수

--- test/소스.cpp
+++ test/소스.cpp
@@ -8,16 +8,23 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, v; // a: 낮 동안 올라가는 거리, b: 밤 동안 미끄러지는 거리, v: 목표 높이
-    scanf("%d %d %d", &a, &b, &v); 
+    int a, b;
 
-    // 1. (v - b): 목표 높이에서 마지막 날의 미끄러짐 b를 뺌, 마지막 날에는 낮에 목표에 도달하면 더 이상 미끄러지지 않기 때문.
-    // 2. (a - b): 낮에 올라간 거리 a에서 밤에 미끄러지는 거리 b를 뺀 순수하게 올라가는 거리.
-    // 3. (v - b - 1): 정수 나눗셈에서 항상 올림을 하기 위해 1을 빼서 조정해줌.
-    // 4. +1: 마지막 날에 미끄러지지 않고 한 번에 올라가는 날을 더해줌.
-    int day = (v - b - 1) / (a - b) + 1;
+    while (1) {  
+        scanf("%d %d", &a, &b);
+        if (a == 0 && b == 0) break; 
 
-    printf("%d\n", day); 
+        if (b % a == 0) {
+            printf("factor\n");
+        }
+        else if (a % b == 0) {
+            printf("multiple\n");
+        }
+        else {
+            printf("neither\n");
+        }
+    }
 
     return 0;
 }
+
