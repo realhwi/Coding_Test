c54121080c4f0ec328a83df8db437f54be8e9640
realhwi <wlsgnl49@gmail.com>
2024-11-01 00:16:27 +0900

    9-2 (약수,배수,소수) 2501번 약수 구하기

--- test/소스.cpp
+++ test/소스.cpp
@@ -8,23 +8,24 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int n, k; // n은 나눌 수, k는 몇 번째인지 
+    scanf("%d %d", &n, &k);
 
-    while (1) {  
-        scanf("%d %d", &a, &b);
-        if (a == 0 && b == 0) break; 
+    int count = 0; // 약수의 개수
+    int result[100] = { 0 };
+    for (int i = 1; i <= n; i++) { // i를 1부터 시작
+        if (n % i == 0) { // 나머지가 0이면 약수
+            result[count] = i;
+            count++; // 약수만 카운트
+        }
+    }
 
-        if (b % a == 0) {
-            printf("factor\n");
-        }
-        else if (a % b == 0) {
-            printf("multiple\n");
-        }
-        else {
-            printf("neither\n");
-        }
+    if (count < k) {
+        printf("0\n");
+    }
+    else {
+        printf("%d\n", result[k - 1]); // 인덱스가 0부터라서 -1
     }
 
     return 0;
 }
-
