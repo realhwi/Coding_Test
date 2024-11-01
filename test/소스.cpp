8f35a9c8141694a02747ebc72c6642152256e1e4
realhwi <wlsgnl49@gmail.com>
2024-11-01 14:01:16 +0900

    9-3 (약수,배수,소수) 9506번 약수들의 합

--- test/소스.cpp
+++ test/소스.cpp
@@ -8,23 +8,42 @@
 #include <stdio.h>
 
 int main() {
-    int n, k; // n은 나눌 수, k는 몇 번째인지 
-    scanf("%d %d", &n, &k);
+    
+    // 완전수의 조건 : n의 반부터 나눠지는 숫자까지의 합이 n과 같다면
+    int n;
 
-    int count = 0; // 약수의 개수
-    int result[100] = { 0 };
-    for (int i = 1; i <= n; i++) { // i를 1부터 시작
-        if (n % i == 0) { // 나머지가 0이면 약수
-            result[count] = i;
-            count++; // 약수만 카운트
+    while (1)
+    {
+        scanf("%d", &n);
+
+        if (n == -1) return 0;
+
+        int sum = 0; // 약수의 합을 저장 
+        int N[100] = {0}; // 약수 저장용 배열
+        int count = 0;   // 약수의 개수를 저장할 변수
+
+        // 1부터 n/2까지 반복하여 약수를 찾고 배열에 저장
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {    // i가 n의 약수일 때
+                N[count++] = i;  // 약수를 배열에 저장하고 count 증가
+                sum += i;       // 약수의 합에 i를 더함
+            }
         }
-    }
 
-    if (count < k) {
-        printf("0\n");
-    }
-    else {
-        printf("%d\n", result[k - 1]); // 인덱스가 0부터라서 -1
+        // 약수의 합이 n과 같으면 완전수
+        if (sum == n) {
+            printf("%d = ",n);
+            for (int i = 0; i < count; i++) { // 저장된 약수들을 출력
+                printf("%d",N[i]);
+                if (i < count - 1) { // 마지막 약수 뒤에 "+" 생략
+                    printf(" + ");
+                }
+            }
+            printf("\n");
+        }
+        else {
+            printf("%d is NOT perfact.\n",n);
+        }
     }
 
     return 0;
