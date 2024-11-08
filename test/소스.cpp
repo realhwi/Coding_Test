6cc8cb110ef2f62a9ec3244f1a49b57531bfe442
realhwi <wlsgnl49@gmail.com>
2024-11-08 20:20:45 +0900

    10-2 (기하) 1085번 직사각형에서 탈출

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,11 +9,22 @@
 
 int main() {
 
-    int a,b;
-    scanf("%d %d",&a,&b);
+    int x,y,w,h;
+    scanf("%d %d %d %d",&x,&y,&w,&h);
 
-    printf("%d",a*b);
+    // 각 경계선까지의 거리 계산
+    int left = x;       // 왼쪽 경계선까지의 거리
+    int right = w - x;  // 오른쪽 경계선까지의 거리
+    int bottom = y;     // 아래쪽 경계선까지의 거리
+    int top = h - y;    // 위쪽 경계선까지의 거리
 
+    // 최소 거리 계산
+    int min = left;
+    if (right < min) min = right;
+    if (bottom < min) min = bottom;
+    if (top < min) min = top;
+
+    printf("%d\n", min);
 
     return 0;
 }
