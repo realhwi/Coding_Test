48d261e6d0daee4288426182a95e5aa0cb03fafc
realhwi <wlsgnl49@gmail.com>
2024-11-08 19:52:57 +0900

    10-1 (기하) 27323번 직사각형

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,19 +9,11 @@
 
 int main() {
 
-    int n; 
-    scanf("%d",&n);
+    int a,b;
+    scanf("%d %d",&a,&b);
 
-    for (int i = 2; i <= n; i++) { // i 2부터 제곱근까지 확인
-        while (n%i==0) // n이 i로 나누어떨어지면
-        {
-            printf("%d\n",i); // i를 소인수로 출력
-            n /= i; // n을 i로 나눔
-        }
-    }
-    if (n > 1) {
-        printf("%d\n",n); // 나누고 남은 n이 소수인 경우 출력 
-    }
+    printf("%d",a*b);
+
 
     return 0;
 }
