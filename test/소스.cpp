81b6b0bddb29108add167756066fd3a0a11d9d72
realhwi <wlsgnl49@gmail.com>
2024-11-11 12:08:04 +0900

    10-3 (기하) 3009번 네 번째 점

--- test/소스.cpp
+++ test/소스.cpp
@@ -9,22 +9,37 @@
 
 int main() {
 
-    int x,y,w,h;
-    scanf("%d %d %d %d",&x,&y,&w,&h);
+    int x[3],y[3]; //세개의 좌표를 입력받아 저장
 
-    // 각 경계선까지의 거리 계산
-    int left = x;       // 왼쪽 경계선까지의 거리
-    int right = w - x;  // 오른쪽 경계선까지의 거리
-    int bottom = y;     // 아래쪽 경계선까지의 거리
-    int top = h - y;    // 위쪽 경계선까지의 거리
+    for (int i = 0; i < 3; i++) {
+        scanf("%d %d", &x[i], &y[i]);
+    }
 
-    // 최소 거리 계산
-    int min = left;
-    if (right < min) min = right;
-    if (bottom < min) min = bottom;
-    if (top < min) min = top;
+    int x1,y1; // x,y 중 각 꼭지점 4개 {x,y} {x,y1} {x1,y} {x1,y1} 이런식의 짝 중 짝이 없는 것을 찾기 
 
-    printf("%d\n", min);
+    //x좌표 중 짝이 없는 것 x1에 넣기 
+    if (x[0] == x[1]) {
+        x1 = x[2];
+    }
+    else if (x[0] == x[2]) {
+        x1 = x[1];
+    }
+    else {
+        x1 = x[0];
+    }
+
+    //y좌표 중 짝이 없는 것 y1에 넣기 
+    if (y[0] == y[1]) {
+        y1 = y[2];
+    }
+    else if (y[0] == y[2]) {
+        y1 = y[1];
+    }
+    else {
+        y1 = y[0];
+    }
+
+    printf("%d %d",x1,y1);
 
     return 0;
 }
