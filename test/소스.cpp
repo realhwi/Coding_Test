53bcdc36d7574ccebf5299f125fb11b9e51f3166
realhwi <wlsgnl49@gmail.com>
2024-11-12 10:35:44 +0900

    10-4 (기하) 15894번 수학은 체육과목입니다

--- test/소스.cpp
+++ test/소스.cpp
@@ -8,38 +8,8 @@
 #include <stdio.h>
 
 int main() {
-
-    int x[3],y[3]; //세개의 좌표를 입력받아 저장
-
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d", &x[i], &y[i]);
-    }
-
-    int x1,y1; // x,y 중 각 꼭지점 4개 {x,y} {x,y1} {x1,y} {x1,y1} 이런식의 짝 중 짝이 없는 것을 찾기 
-
-    //x좌표 중 짝이 없는 것 x1에 넣기 
-    if (x[0] == x[1]) {
-        x1 = x[2];
-    }
-    else if (x[0] == x[2]) {
-        x1 = x[1];
-    }
-    else {
-        x1 = x[0];
-    }
-
-    //y좌표 중 짝이 없는 것 y1에 넣기 
-    if (y[0] == y[1]) {
-        y1 = y[2];
-    }
-    else if (y[0] == y[2]) {
-        y1 = y[1];
-    }
-    else {
-        y1 = y[0];
-    }
-
-    printf("%d %d",x1,y1);
-
+    long long int n;
+    scanf("%d", &n);
+    printf("%d", n * 4); // 실선 개수 구하려면 4개씩 증가함 
     return 0;
 }
