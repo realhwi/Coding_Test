dfcd4818fdca4825a4434f30c56f722b7bce4b10
realhwi <wlsgnl49@gmail.com>
2024-11-14 23:54:28 +0900

    10-6 (기하) 10101번 삼각형 외우기

--- test/소스.cpp
+++ test/소스.cpp
@@ -7,41 +7,26 @@
 #include <stdlib.h>
 
 int main() {
-    int n, x, y;
+    
+    int n[3]={0};
+    int sum = 0;
 
-    // 최소값 최대값 초기화 
-    int min_x = 10000, max_x = -10000;  
-    int min_y = 10000, max_y = -10000;
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &n[i]);
+        sum+=n[i];
+    }
 
-    scanf("%d", &n);
-
-    // 점이 하나일 경우 넓이 0
-    if (n == 1) {
-        printf("0\n");
+    if (sum != 180) {
+        printf("Error\n");
+    }
+    else if (n[0] == 60 && n[1] == 60 && n[2] == 60) {
+        printf("Equilateral\n");
+    }
+    else if (n[0] == n[1] || n[1] == n[2] || n[0] == n[2]) {
+        printf("Isosceles\n");
     }
     else {
-        for (int i = 0; i < n; i++) {
-            // 각 점의 좌표 입력
-            scanf("%d %d", &x, &y);
-
-            // x 좌표의 최대값과 최소값
-            if (x > max_x) {
-                max_x = x;
-            }
-            if (x < min_x) {
-                min_x = x;
-            }
-
-            // y 좌표의 최대값과 최소값 
-            if (y > max_y) {
-                max_y = y;
-            }
-            if (y < min_y) {
-                min_y = y;
-            }
-        }
-        // 최소 크기의 직사각형 넓이 출력
-        printf("%d\n", (max_x - min_x) * (max_y - min_y));
+        printf("Scalene\n");
     }
 
     return 0;
