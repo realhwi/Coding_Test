e0bf760dceff8dc5078653c53da28f2527f5cdb0
realhwi <wlsgnl49@gmail.com>
2024-11-19 14:08:47 +0900

    11-1 (시간복잡도) 24262번 알고리즘의 수행 시간1

--- test/소스.cpp
+++ test/소스.cpp
@@ -4,28 +4,15 @@
 #include <ctime>
 #include <cstring>
 #include <vector>
-#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int n;
+    scanf("%d", &n);  
 
-    // 삼각형의 조건 : 가장 긴 변 < 다른 두 변의 합
-    // 삼각형의 조건에서 벗어나면 그 변의 길이를 줄여야함 (가장 긴 변 >= 다른 두 변일 때)
-    while (a >= b + c || b >= a + c || c >= a + b) {
-        // 가장 긴 변을 찾아서 1 줄임 (조건 만족할때까지)
-        if (a >= b && a >= c) {
-            a--;
-        }
-        else if (b >= a && b >= c) {
-            b--;
-        }
-        else {
-            c--;
-        }
-    }
-
-    printf("%d\n", a + b + c);
+    // 함수에서 코드는 항상 한번만 수행됨 
+    printf("1\n");  // 코드 1의 수행 횟수 
+    printf("0\n");  // 시간 복잡도는 O(1) = 최고차항 0 
 
     return 0;
 }
