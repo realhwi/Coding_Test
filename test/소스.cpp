e087b94bab43d1eaef32f2f27f5e583b25b00ef3
realhwi <wlsgnl49@gmail.com>
2024-11-22 17:46:05 +0900

    11-3 (시간복잡도) 알고리즘의 수행 시간 3

--- test/소스.cpp
+++ test/소스.cpp
@@ -7,11 +7,13 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);  
+    long long n; //제곱수 int자료형(4바이트)이 아닌 longlong으로 (8바이트)
 
-    printf("%d\n",n);  // 코드 1의 수행 횟수 
-    printf("1\n");  // 시간 복잡도는 o(n) = 최고차항 1
+    scanf("%lld", &n); //ll형식 지정자 사용해 입력받기 
+
+
+    printf("%d\n",n*n);  // 코드 1의 수행 횟수 
+    printf("2\n");  // 시간 복잡도는 o(n제곱) = 최고차항 
 
     return 0;
 }
